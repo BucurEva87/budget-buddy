716c0deeb91d63217a037ad08f61f898e6b3e5d8
BucurEva87 <bucur.eva87@gmail.com>
2023-03-20 17:01:57 +0200

    Add validation to Entry model

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,6 @@
 class Entry < ApplicationRecord
   belongs_to :author
+
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :amount, presence: true, numericality: { greater_than: 0 }
 end
