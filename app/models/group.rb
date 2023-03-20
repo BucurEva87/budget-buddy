5e20569d202d117b36ead73738ade30857b223ff
BucurEva87 <bucur.eva87@gmail.com>
2023-03-20 17:07:06 +0200

    Add validation to Group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,4 @@
 class Group < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 2, maximum: 50 }
+  validates :icon, presence: true, format: { with: /\A#{URI::regexp(['http', 'https'])}\z/, message: "must be a valid URL" }
 end
