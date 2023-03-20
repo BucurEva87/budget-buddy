6821d84ac12b30f2dd2f17d0bc0dfdeae59dad43
BucurEva87 <bucur.eva87@gmail.com>
2023-03-20 17:10:21 +0200

    Modify code to abide linter's rules

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,8 @@
 class Group < ApplicationRecord
   validates :name, presence: true, length: { minimum: 2, maximum: 50 }
-  validates :icon, presence: true, format: { with: /\A#{URI::regexp(['http', 'https'])}\z/, message: "must be a valid URL" }
+  validates :icon, presence: true,
+                   format: {
+                     with: /\A#{URI::DEFAULT_PARSER.make_regexp(%w[http https])}\z/,
+                     message: 'must be a valid URL'
+                   }
 end
