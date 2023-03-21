a8ff0c8f6a9c91c39e900d1f619bd8567d40cc7d
BucurEva87 <bucur.eva87@gmail.com>
2023-03-21 11:46:56 +0200

    Add photo field to user

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_20_153329) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", limit: 50, null: false
+    t.string "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
