516ff13934c7f932d3ffe9ac252597c44a8589bc
BucurEva87 <bucur.eva87@gmail.com>
2023-03-22 20:40:16 +0200

    Modify code to abide linter's rules

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  
+
   has_one_attached :photo
 
-  after_commit :add_default_photo, on: [:create, :update]
+  after_commit :add_default_photo, on: %i[create update]
 
   validates :name, presence: true, length: { minimum: 2, maximum: 50 }
 
@@ -15,16 +15,16 @@ class User < ApplicationRecord
   private
 
   def add_default_photo
-    unless photo.attached?
-      photo.attach(
-        io: File.open(
-          Rails.root.join(
-            'app', 'assets', 'images', 'user_default.png'
-          )
-        ),
-        filename: 'user_default.png',
-        content_type: 'image/png'
-      )
-    end
+    return if photo.attached?
+
+    photo.attach(
+      io: File.open(
+        Rails.root.join(
+          'app', 'assets', 'images', 'user_default.png'
+        )
+      ),
+      filename: 'user_default.png',
+      content_type: 'image/png'
+    )
   end
 end
