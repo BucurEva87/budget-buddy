511485695e62bba7062d62cfe8a16d423d69f901
BucurEva87 <bucur.eva87@gmail.com>
2023-03-23 10:55:01 +0200

    Add Category page

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,25 @@
-class Group < ApplicationRecord
+class Group < ApplicationRecord  
+  has_many :entries, dependent: :destroy
+
+  has_one_attached :icon
+
+  after_commit :add_default_icon, on: [:create, :update]
+
   validates :name, presence: true, length: { minimum: 2, maximum: 50 }
-  validates :icon, presence: true,
-                   format: {
-                     with: /\A#{URI::DEFAULT_PARSER.make_regexp(%w[http https])}\z/,
-                     message: 'must be a valid URL'
-                   }
+
+  private
+
+  def add_default_icon
+    return if icon.attached?
+
+    icon.attach(
+      io: File.open(
+        Rails.root.join(
+          'app', 'assets', 'images', 'group_default.png'
+        )
+      ),
+      filename: 'group_default.png',
+      content_type: 'image/png'
+    )
+  end
 end
